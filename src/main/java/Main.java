bce3363f8444af1f0244b8a0d532d29db1570d7b
thanhkhon03 <2121050666@student.humg.edu.vn>
2025-01-16 14:31:11 +0700

    2121050666

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,12 @@ public class Main {
     	int myInt = 42;
     	double myDouble = 3.14;
     	String myString = "Hello, Java!";
+    	
+    	
+         System.out.println(myInt);
+         System.out.println(myDouble);
+         System.out.println(myString);
+    	
     			
     	if (myInt > 10) {
     	   System.out.println("Greater than 10");
